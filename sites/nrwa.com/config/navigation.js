560fcd69cef5eb9d3842c50ac7835e67b07fbd18
Brandon Krigbaum <brandonbk@gmail.com>
2021-10-25 15:38:01 -0500

    Add navigation config

---
+++ sites/nrwa.com/config/navigation.js
@@ -0,0 +1,70 @@
+const topics = {
+  primary: [
+    { href: '/utility-operations', label: 'Utility Operations' },
+    { href: '/legislation', label: 'Legislation' },
+    { href: '/technology', label: 'Technology' },
+  ],
+  expanded: [],
+  secondary: [],
+};
+
+const resources = [
+  { href: '/magazine', label: 'Magazine' },
+  { href: '/videos', label: 'Videos' },
+  { href: '/resource-guide', label: 'Resource Guide' },
+  { href: 'https://careers.nrwa.org', label: 'Career Center', target: '_blank' },
+  // { href: '/white-papers', label: 'White Papers' },
+];
+
+const utilities = [
+  { href: '/page/advertise', label: 'Advertise' },
+  { href: '/page/contact-us', label: 'Contact Us' },
+];
+
+const mobileMenu = {
+  primary: topics.primary,
+  secondary: [
+    ...resources,
+  ],
+};
+
+const desktopMenu = {
+  about: [...utilities],
+  sections: [
+    ...topics.primary,
+    ...topics.expanded,
+    ...topics.secondary,
+  ],
+  resources: [...resources],
+};
+
+module.exports = {
+  primary: {
+    items: [],
+  },
+  secondary: {
+    items: [
+      ...topics.primary,
+      { href: '/resource-guide', label: 'Resource Guide' },
+      { href: 'https://careers.nrwa.org', label: 'Career Center', target: '_blank' },
+      { href: 'https://waterproconference.org', label: 'WaterPro Conference', target: '_blank' },
+    ],
+  },
+  tertiary: {
+    items: [],
+  },
+  desktopMenu,
+  mobileMenu,
+  footer: {
+    items: [
+      { href: '/page/advertise', label: 'Advertise' },
+      { href: '/page/contact-us', label: 'Contact Us' },
+      { href: '/page/privacy-policy', label: 'Privacy Policy' },
+      { href: '/site-map', label: 'Site Map' },
+    ],
+    topics: topics.primary,
+    more: [
+      ...utilities,
+    ],
+  },
+};
