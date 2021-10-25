f31d0575e69e5ba3f85de41726746c67e872e883
Brandon Krigbaum <brandonbk@gmail.com>
2021-10-25 18:30:33 -0500

    Remove omeda references

--- packages/global/routes/identity-x.js
+++ packages/global/routes/identity-x.js
@@ -1,9 +1,7 @@
 const gql = require('graphql-tag');
 const IdentityX = require('@parameter1/base-cms-marko-web-identity-x');
-const { getAsObject, get } = require('@parameter1/base-cms-object-path');
+const { getAsObject } = require('@parameter1/base-cms-object-path');
 const { asyncRoute } = require('@parameter1/base-cms-utils');
-const rapidIdentify = require('@parameter1/base-cms-marko-web-omeda-identity-x/routes/rapid-identify');
-const omedaConfig = require('../config/omeda');
 const authenticate = require('../templates/user/authenticate');
 const login = require('../templates/user/login');
 const logout = require('../templates/user/logout');
@@ -41,11 +39,6 @@ module.exports = (app) => {
   app.get(config.getEndpointFor('profile'), (_, res) => {
     res.marko(profile);
   });
-
-  app.use('/__idx/omeda-rapid-ident', rapidIdentify({
-    brandKey: omedaConfig.brandKey,
-    productId: get(omedaConfig, 'rapidIdentification.productId'),
-  }));
 
   app.get('/__idx/comment-count/:identifier', asyncRoute(async (req, res) => {
     const { identityX } = req;
