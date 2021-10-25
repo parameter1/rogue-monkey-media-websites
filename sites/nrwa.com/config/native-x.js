534ccfa80808bb8bb1962c6fafa4447039356980
Brandon Krigbaum <brandonbk@gmail.com>
2021-10-25 13:09:05 -0500

    Add native-x config

---
+++ sites/nrwa.com/config/native-x.js
@@ -0,0 +1,12 @@
+const configureNativeX = require('@rogue-monkey-media/package-global/config/native-x');
+
+const config = configureNativeX();
+
+config.enabled = false;
+
+config
+  .setAliasPlacements('default', [
+    { name: 'default', id: 'NOT_SET' },
+  ]);
+
+module.exports = config;
