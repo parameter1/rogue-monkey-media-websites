3558d1c238e7bc4b899917cc3e6f13ae79481a5d
Brian Miller <brian@parameter1.com>
2021-10-29 08:44:36 -0500

    Udate header navigation with new items and toolbox
    Update header layout with new header items and adjust spacing on header items in primary and secondary navigation.

--- sites/content.nrwa.org/config/navigation.js
+++ sites/content.nrwa.org/config/navigation.js
@@ -3,9 +3,17 @@ const topics = {
     { href: '/utility-operations', label: 'Utility Operations' },
     { href: '/legislation', label: 'Legislation' },
     { href: '/technology', label: 'Technology' },
+    { href: '/resource-guide', label: 'Resource Guide' },
+    { href: 'https://careers.nrwa.org/', label: 'Career Center' },
   ],
   expanded: [],
-  secondary: [],
+  secondary: [
+    { href: 'https://nrwa.org/', label: 'NRWA.org' },
+    { href: 'https://wateruniversity.org/', label: 'Water University' },
+    { href: 'https://careers.nrwa.org/', label: 'Career Center' },
+    { href: 'https://members.nrwa.org/Shared_Content/Sign_In.aspx', label: 'Member Portal' },
+    { href: 'https://www.waterproconference.org/', label: 'WaterPro Conference' },
+  ],
 };
 
 const resources = [
@@ -13,7 +21,6 @@ const resources = [
   { href: '/videos', label: 'Videos' },
   { href: '/resource-guide', label: 'Resource Guide' },
   { href: 'https://careers.nrwa.org', label: 'Career Center', target: '_blank' },
-  // { href: '/white-papers', label: 'White Papers' },
 ];
 
 const utilities = [
@@ -22,7 +29,11 @@ const utilities = [
 ];
 
 const mobileMenu = {
-  primary: topics.primary,
+  primary: [
+    ...topics.primary,
+    ...topics.secondary,
+
+  ],
   secondary: [
     ...resources,
   ],
@@ -33,21 +44,29 @@ const desktopMenu = {
   sections: [
     ...topics.primary,
     ...topics.expanded,
-    ...topics.secondary,
   ],
-  resources: [...resources],
+  toolbox: [
+    { href: '/resource-guide', label: 'Resource Guide' },
+    { href: 'https://www.ruralwatermag.com/ruralwater/third_quarter_2021_vol__42_no_3/MobilePagedArticle.action?articleId=1721459#articleId1721459', label: 'Rural Water Magazine', target: '_blank' },
+    { href: 'https://theassociationpartner.com/nrwa/', label: 'Digital Retargeting Opportunities', target: '_blank' },
+    { href: 'http://nrwa.org/non-discrimination-policy/', label: 'Non-Discrimination Policy', target: '_blank' },
+    { href: 'https://members.nrwa.org/Public/Members/NRWA-Store/iCore/Store/StoreLayouts/Store_Home.aspx', label: 'NRWA Online Store', target: '_blank' },
+    { href: 'http://nrwa.org/wp-content/uploads/2021/02/2021-NRWA-MediaKit-FINAL-1.pdf', label: 'Media Kit', target: '_blank' },
+  ],
 };
 
 module.exports = {
   primary: {
-    items: [],
+    items: [
+      ...topics.secondary,
+    ],
   },
   secondary: {
     items: [
       ...topics.primary,
-      { href: '/resource-guide', label: 'Resource Guide' },
-      { href: 'https://careers.nrwa.org', label: 'Career Center', target: '_blank' },
-      { href: 'https://waterproconference.org', label: 'WaterPro Conference', target: '_blank' },
+      // { href: '/resource-guide', label: 'Resource Guide' },
+      // { href: 'https://careers.nrwa.org', label: 'Career Center', target: '_blank' },
+      // { href: 'https://waterproconference.org', label: 'WaterPro Conference', target: '_blank' },
     ],
   },
   tertiary: {
