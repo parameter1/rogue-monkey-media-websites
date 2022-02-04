043b0eb0c8eb9851292c09ccea68bf32c5c3cfb7
Shinsina <shinsina@att.net>
2022-02-04 15:10:03 -0600

    Supply contentTypes list for searchConfig.

--- sites/content.nrwa.org/config/search.js
+++ sites/content.nrwa.org/config/search.js
@@ -1,4 +1,5 @@
 module.exports = {
+  contentTypes: ['Article', 'Blog', 'Company', 'News', 'Podcast', 'PressRelease', 'Product', 'Video', 'Whitepaper'],
   assignedToWebsiteSectionIds: [
     82655,
     82648,
