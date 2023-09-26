740578c200ea177a00d3ac96541adfcb91c02a6f
Jacob Bare <jacob@parameter1.com>
2023-09-26 15:52:20 -0500

    Reconfigure nativex to use compat api & enable by default

--- packages/global/config/native-x.js
+++ packages/global/config/native-x.js
@@ -1,6 +1,6 @@
 const NativeXConfiguration = require('@parameter1/base-cms-marko-web-native-x/config');
 
 module.exports = ({
-  uri = 'https://roguemonkeymedia.native-x.parameter1.com',
-  enabled = false,
-} = {}) => new NativeXConfiguration(uri, { enabled });
+  uri = 'https://delivery.mindfulcms.com/rmm/default/compat/native-website',
+  enabled = true,
+} = {}) => new NativeXConfiguration(uri, { enabled, tenantKey: 'roguemonkeymedia' });
