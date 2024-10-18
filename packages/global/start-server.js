8355d3e35f7bc3e1bfff19a403ac3e9c68f348a9
Brian Miller <brian@parameter1.com>
2024-10-18 15:24:18 -0500

    Add contentGating and newsletterModal middlewares

--- packages/global/start-server.js
+++ packages/global/start-server.js
@@ -3,6 +3,8 @@ const { startServer } = require('@parameter1/base-cms-marko-web');
 const { set, get } = require('@parameter1/base-cms-object-path');
 const loadInquiry = require('@parameter1/base-cms-marko-web-inquiry');
 const htmlSitemapPagination = require('@parameter1/base-cms-marko-web-html-sitemap/middleware/paginated');
+const contentGating = require('@parameter1/base-cms-marko-web-theme-monorail/middleware/content-gating');
+const newsletterModalState = require('@parameter1/base-cms-marko-web-theme-monorail/middleware/newsletter-modal-state');
 
 const document = require('./components/document');
 const components = require('./components');
@@ -50,6 +52,12 @@ module.exports = (options = {}) => {
 
       // Use paginated middleware
       app.use(htmlSitemapPagination());
+
+      // Use newsletterModalState middleware
+      app.use(newsletterModalState());
+
+      // Install custom content gating middleware
+      contentGating(app);
 
       // Setup GAM.
       const gamConfig = get(options, 'siteConfig.gam');
